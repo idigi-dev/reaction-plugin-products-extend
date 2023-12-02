a9a0098af9f4e23fbf8d09b4d78f55211502b1c4
El Mehdi <mehdi.karbal@gmail.com>
2023-12-02 22:34:28 +0100

    pId & vId

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -7,4 +7,25 @@ import Query from "./Query/index.js";
 
 export default {
   Query,
+  CatalogProduct: { pId: (n) => n.productId },
+  CatalogProductVariant: { vId: (n) => n.variantId },
+  ProductPricingInfo: {
+    ...["maxPrice", "minPrice", "price"].reduce(
+      (p, k) => ({
+        ...p,
+        [k]: (n) => {
+          if (typeof n[k] === "number") {
+            return n[k];
+          }
+          console.info(
+            `\n»» ${k}`,
+            n[k],
+            `\n••••••| ${new Date().toLocaleString()} |••••••\n`
+          );
+          return 0.0;
+        },
+      }),
+      {}
+    ),
+  },
 };
